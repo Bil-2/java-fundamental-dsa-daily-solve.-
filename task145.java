e1225f93f81d023f16d1c1a736e65ed0313d81c4
Bil-2 <biltubag29@gmail.com>
2026-08-27 10:42:13 +0530

    Changes to be committed:

--- task145.java
+++ task145.java
@@ -1,6 +1,6 @@
 public class task145 {//rumbus pattern printing//
   public static void main(String[] args) {
-    int n = 5;
+    int n = 500;
     for (int row = 1;  row <=n; row++) {
       for (int col = 1; col<=n - row;col++) {
         System.out.print(" ");
