e09f36003493e932a66d6b5271cc14a1e8e4ee31
Bil-2 <biltubag29@gmail.com>
2026-08-31 23:32:38 +0530

    Changes to be committed:

--- task147.java
+++ task147.java
@@ -1,7 +1,13 @@
 public class task147 {//array basic 
   public static void main(String[]args){
     int a [] = {1,2,3,4,5};
-    System.out.println(a[4]);
+    System.out.println(a[4]+" ");
+    for (int b:a){// [ 1 ]diff way of printing array elements
+      System.out.println(b+" ");
+      for (int i=0;i<a.length;i++){ //[ 2] 
+        System.out.println(a[i]+" ");
+      }
+    }
   }
   
 }
---
+++ task149.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class task149 {//tostring method
+  public static void main(String[] args) {
+    String a[] = {"apple", "banana", "cherry","coconut"};
+    System.out.println(Arrays.toString(a));
+    System.out.println(Arrays.asList(a));//toList method
+  }
+} 
